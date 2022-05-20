5a4fdfad1d93333722a29b6dad48d8eaf23d4a30
Abighail Shafira Ihsani <77433598+abighailshafira@users.noreply.github.com>
2022-05-20 17:49:32 +0700

    Update Fasilitas.dart

--- lib/Fasilitas.dart
+++ lib/Fasilitas.dart
@@ -1,11 +1,140 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'RincianFasilitas.dart';
 
 class Fasilitas extends StatelessWidget {
   const Fasilitas({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('fasilitas!'),
+    return Center(
+      child: ListView(padding: EdgeInsets.all(20), shrinkWrap: true, children: [
+        InkWell(
+          child: Container(
+            decoration: BoxDecoration(border: Border.all()),
+            padding: EdgeInsets.all(14),
+            child: Row(children: <Widget>[
+              Expanded(
+                flex: 3,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        "Balai Bahasa",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "Pusat Pembelajaran",
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Image.network(
+                  "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg", //pastikan file ada di assets/images
+                  height: 50,
+                ),
+              )
+            ]),
+          ),
+          onTap: () {
+            //gunakan navigator untuk panggil RincianFasilitas
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return RincianFasilitas();
+            }));
+          },
+        ),
+        Container(
+          decoration: BoxDecoration(border: Border.all()),
+          padding: EdgeInsets.all(14),
+          child: Row(children: <Widget>[
+            Expanded(
+              flex: 3,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: <Widget>[
+                    Text("Gymnasium",
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                    Text(
+                      "Olahraga",
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Image.network(
+                "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg", //pastikan file ada di assets/images
+                height: 50,
+              ),
+            )
+          ]),
+        ),
+        Container(
+          decoration: BoxDecoration(border: Border.all()),
+          padding: EdgeInsets.all(14),
+          child: Row(children: <Widget>[
+            Expanded(
+              flex: 3,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: <Widget>[
+                    Text("Stadion UPI",
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                    Text(
+                      "Olahraga",
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Image.network(
+                "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg", //pastikan file ada di assets/images
+                height: 50,
+              ),
+            )
+          ]),
+        ),
+        Container(
+          decoration: BoxDecoration(border: Border.all()),
+          padding: EdgeInsets.all(14),
+          child: Row(children: <Widget>[
+            Expanded(
+              flex: 3,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: <Widget>[
+                    Text("UPInet",
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                    Text(
+                      "Teknologi Informasi",
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Image.network(
+                "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg", //pastikan file ada di assets/images
+                height: 50,
+              ),
+            )
+          ]),
+        ),
+      ]),
     );
   }
 }
