a66f7e074c5831c67ffaca8cb96f92e0f528f248
Abighail Shafira Ihsani <77433598+abighailshafira@users.noreply.github.com>
2022-05-20 17:50:41 +0700

    Add files via upload

---
+++ lib/RincianFasilitas.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+
+class RincianFasilitas extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Rincian Fasilitas'),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(
+                  top: 25.0, bottom: 25.0, left: 25.0, right: 25.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15.0),
+                child: Image.network(
+                  "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg",
+                  height: 200,
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 25.0),
+              child: Text(
+                "Balai Bahasa",
+                style: const TextStyle(
+                    fontSize: 20.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 25.0, right: 25.0),
+              child: Text(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                textAlign: TextAlign.justify,
+                style: const TextStyle(
+                  fontSize: 16.0,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
